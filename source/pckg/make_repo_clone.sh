9c415824221badc1f824505a682eaf98e18037a4
ssagan077 <ssagan077@gmail.com>
2023-05-10 00:21:13 +0000

    Adding starter scripts

---
+++ source/pckg/make_repo_clone.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Check the script is being run by current user(no root)
+if [ "$(id -u)" = "0" ]; then
+   echo "This script must be run as current user, not root"
+   exit 1
+fi
+
+op_user="ssagan077"
+git_dir="/home/${op_user}/project-sb"
+pckg_dir="${git_dir}/packages"
+
+#Checking git
+if dpkg -l git; then echo "git has not been installed. Installing..."
+   sudo apt-get install git && echo "Git has been installed" || exit 1
+fi
+
+#Create pub and secret ssh keys for exchanging with OVPN-server
+if [ ! -f /home/${op_user}/.ssh/id_rsa.pub ]; then
+  echo "Creating ssh keys..."
+  ssh-keygen -t rsa && echo "ssh keys created" || exit 1
+fi
+
+echo -e "Please, copy this public key to clipboard and add to your repository on github:\n"
+cat /home/${op_user}/.ssh/id_rsa.pub
+sleep 3
+
+read -p "Did you add your public key to GitHub? y/n: " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+   echo "Ok, let's create repository"
+else
+   echo "Next time"; exit 1
+fi
+
+if [ -d "${git_dir}/packages" ]; then
+   echo "It seems that the directory ${git_dir}/packages has already exists. Please, check it..."
+   exit 1
+fi
+
+cd /home/${op_user}
+
+git config --global user.name "$op_user"
+git config --global user.email "${op_user}@gmail.com"
+git config --global core.editor nano
+
+
+if git clone git@github.com:"${op_user}/project-sb.git"
+then echo "Local Repository has been created"
+else
+   echo "Something went wrong...Try it manually"
+   exit 1
+fi
+
+echo "Now you should run the script start_pckg.sh"
---
+++ source/pckg/start_pckg.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# Check the script is being run by root (with sudo)
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+pckg_address="10.88.10.5"
+op_user="ssagan077"
+data_dir="/home/${op_user}/project-sb"
+rep_dir="/var/www/html/debian"
+pckg_dir="${data_dir}/packages"
+pckg_server="vm-backup-srv"
+domain_name="sigmanet.site"
+
+# Checking password
+echo -e "\nYou have to set a user password to continue. If you have not done so, please abort this script and do it now."
+read -p "Interrupt script execution to set password? y/n: " -n 1 -r
+   echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo -e "\nPlease execute command: sudo passwd _username_"
+      exit 1
+fi
+
+echo "Updating packages..."
+apt-get update &>/dev/null && echo "repo list updated" || exit 1
+
+#Installing nginx
+if dpkg -l nginx &>/dev/null; then echo "nginx has been already installed"
+else
+     apt-get install nginx && echo "Our package installed" || exit 1
+fi
+
+#Installing git
+if dpkg -l git &>/dev/null; then echo "git has been already installed"
+else
+     apt-get install git && echo "Our package installed" || exit 1
+fi
+
+if [ ! -d "${pckg_dir}" ]; then
+  echo "There is no directory ${pckg_dir}. Please run make_repo_clone.sh first!"
+  exit 1
+fi
+
+# Checking our directory with deb-packages
+if [ $(find $pckg_dir/ -type f -name *.deb | wc -l) -eq 0 ]; then
+   echo "There are no deb-packages in our repository ($pckg_dir). Please check it "
+   exit 1
+fi
+
+
+#Adding hostname to HOST
+if [ $(grep $pckg_address" ${pckg_server}" /etc/hosts | wc -l ) -eq 0 ]; then
+    echo $pckg_address" ${pckg_server}.${domain_name}" >> /etc/hosts
+fi
+
+
+echo "Checking local repository dir..."
+[ ! -d  $rep_dir ] && mkdir $rep_dir
+
+# Request for clean deb-reposotory
+if [ $(find $rep_dir/ -type f | wc -l) -gt 0 ]; then
+     read -p "Delete all deb-packages in our repository ($rep_dir) before creating new list? y/n: " -n 1 -r
+     if [[ $REPLY =~ ^[Yy]$ ]]; then rm -f $rep_dir/* ; fi
+fi
+
+# Copying our packages to repository from directory ~/packages/
+echo -e "\nCopying files to repository..."
+
+find $pckg_dir -type f -name *.deb -exec cp {} $rep_dir/ \; || exit 1
+
+#dpkg -l dpkg-dev &>/dev/null || apt-get install dpkg-dev && echo "dpkg-dev installed"
+apt-get install dpkg-dev && echo "dpkg-dev installed"
+
+echo "Creating list of packages..."
+cd $rep_dir
+dpkg-scanpackages . | gzip -c9 > Packages.gz && echo "Packages list has created!" || exit 1
+
+echo "Adding our repo to Repo list..."
+if [ $(grep "http://${pckg_server}.${domain_name}" /etc/apt/sources.list | wc -l) -eq 0 ]; then
+    echo "deb [trusted=yes] http://${pckg_server}.${domain_name}/debian ./" | tee -a /etc/apt/sources.list > /dev/null
+fi
+
+echo "Updating repo list..."
+apt-get update || exit 1
+
+echo "Now we can install deb-packages from our repository"
+
+#Installing package that deploying PCKG-server
+echo -e "\nInstalling package deploy-pckg..."
+if dpkg -l deploy-pckg &>/dev/null; then echo "deploy-pckg has been already installed"
+else
+     apt-get install deploy-pckg && echo "package deploy-pckg installed" || exit 1
+fi
+
+##Our Variables
+source pckg_paths.sh
+
+#Adding hostnames to HOST
+if [ $(grep "${mon_address} ${mon_server}" /etc/hosts | wc -l ) -eq 0 ]; then
+    echo "${mon_address} ${mon_server}.${domain_name}" >> /etc/hosts
+fi
+if [ $(grep "${ovpn_address} ${ovpn_server}" /etc/hosts | wc -l ) -eq 0 ]; then
+    echo "${ovpn_address} ${ovpn_server}.${domain_name}" >> /etc/hosts
+fi
+if [ $(grep "${pckg_address} ${pckg_server}" /etc/hosts | wc -l ) -eq 0 ]; then
+    echo "${pckg_address} ${pckg_server}.${domain_name}" >> /etc/hosts
+fi
+
+echo "Checking installed software..."
+check_installed "nginx"
+check_installed "openssl"
+check_installed "prometheus-node-exporter"
+check_installed "prometheus-nginx-exporter"
+check_installed "iptables-persistent"
+
+echo "To continue, please run script prepare_net_pckg.sh"
